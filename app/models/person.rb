48f58745c5c992fbdac5fe0ec9a38e8d1cbd4121
Kevin McHugh <kev@kevinmchugh.me>
2014-11-26 23:22:29 -0600

    Add Person#up_by

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+
+  has_many :checkins
+  def up_by
+    starting_weight = checkins.first.try(:weight)
+    return nil unless starting_weight
+    current_weight  = checkins.last.try(:weight) || 0
+    current_weight - starting_weight
+  end
 end
