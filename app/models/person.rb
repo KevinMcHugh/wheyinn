f15a1d61d75ed16030e6d3cf936b90719a9c7720
Kevin McHugh <kev@kevinmchugh.me>
2014-11-27 00:56:24 -0600

    Added percentage change

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,19 @@
 class Person < ActiveRecord::Base
 
   has_many :checkins
+
   def up_by
-    starting_weight = checkins.first.try(:weight)
     return nil unless starting_weight
     current_weight  = checkins.last.try(:weight) || 0
     current_weight - starting_weight
+  end
+
+  def percentage_change
+    starting_weight ? '%.2f' % (up_by / starting_weight * 100) : nil
+  end
+
+  private
+  def starting_weight
+    checkins.first.try(:weight)
   end
 end
