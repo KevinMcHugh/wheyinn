f7fde61242dfd62b14395b1a618bbcb5705e3970
Kevin McHugh <kevin.mchugh@risk.io>
2014-11-27 10:13:00 -0600

    Add People#show

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,6 +12,17 @@ class Person < ActiveRecord::Base
     starting_weight ? '%.2f' % (up_by / starting_weight * 100) : nil
   end
 
+  def checkin_diffs
+    prev = starting_weight
+    diffs = checkins.map(&:weight).map do |w|
+      n = w - prev
+      prev = w
+      n
+    end
+    diffs = diffs.last(diffs.length - 1)
+    diffs.map { |bd| '%.2f' % bd }
+  end
+
   private
   def starting_weight
     checkins.first.try(:weight)
