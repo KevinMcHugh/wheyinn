f4b07b833ad19c065ba29446fbb6624cf7c0b7da
Kevin McHugh <kevin.mchugh@risk.io>
2014-11-27 00:56:24 -0600

    Added percentage change

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 describe Person do
-
+  subject { Person.create }
+  let(:c1) { Checkin.create(person: subject, weight: 100)}
+  let(:c2) { Checkin.create(person: subject, weight: 200)}
+  let(:c3) { Checkin.create(person: subject, weight: 300)}
   describe '#up_by' do
-    subject { Person.create }
-    let(:c1) { Checkin.create(person: subject, weight: 100)}
-    let(:c2) { Checkin.create(person: subject, weight: 200)}
-    let(:c3) { Checkin.create(person: subject, weight: 300)}
-
     context 'with 0 checkins' do
       it 'returns nil' do
         expect(subject.up_by).to be_nil
@@ -29,6 +27,31 @@ describe Person do
       before { c1; c2; c3}
       it 'calculates the difference between first and last Checkin' do
         expect(subject.up_by).to eql(200)
+      end
+    end
+  end
+  describe '#percentage_change' do
+   context 'with 0 checkins' do
+      it 'returns nil' do
+        expect(subject.percentage_change).to be_nil
+      end
+    end
+    context 'with one checkin' do
+      before { c1 }
+      it 'returns 0' do
+        expect(subject.percentage_change).to eq('0.00')
+      end
+    end
+    context 'with two checkins' do
+      before { c1; c2 }
+      it 'calculates the difference between first and last Checkin' do
+        expect(subject.percentage_change).to eq('100.00')
+      end
+    end
+    context 'with many checkins' do
+      before { c1; c2; c3}
+      it 'calculates the difference between first and last Checkin' do
+        expect(subject.percentage_change).to eq('200.00')
       end
     end
   end
