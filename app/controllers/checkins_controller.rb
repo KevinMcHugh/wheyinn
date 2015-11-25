e0e374f46d8e1d9e8d86d7e086a0383165e5fc06
Kevin McHugh <kev@kevinmchugh.me>
2015-11-25 00:04:14 -0600

    KEEP MAKING THINGS LOOK NICE

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -4,6 +4,9 @@ class CheckinsController < ApplicationController
     person = Person.find(checkin_params[:person_id])
     @checkin = CreateCheckin.call(person, Event.last, checkin_params[:weight].to_f)
     redirect_to people_path
+  rescue
+    flash[:error] = "Please fill out all fields"
+    redirect_to new_checkin_path
   end
 
   def new
