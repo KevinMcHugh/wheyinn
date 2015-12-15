ffb4da7cd1b8ea30dce47dadeca698494a58f388
Kevin McHugh <kev@kevinmchugh.me>
2015-12-14 21:10:58 -0600

    Adds QUIET_MODE

--- app/interactors/create_checkin.rb
+++ app/interactors/create_checkin.rb
@@ -1,6 +1,7 @@
 class CreateCheckin
 
   def self.call(person, event, weight)
+    return if ENV['QUIET_MODE']
     previous_checkin = Checkin.where(person: person, event: event).first
     delta = nil
     delta = weight - previous_checkin.weight if previous_checkin
