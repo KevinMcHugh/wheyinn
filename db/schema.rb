028cc9588829f782869a826ff995e733360b80b9
Kevin McHugh <kev@kevinmchugh.me>
2016-11-19 15:14:48 -0600

    Adds taglines to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119193652) do
+ActiveRecord::Schema.define(version: 20161119211205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161119193652) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "tagline"
   end
 
   create_table "people", force: true do |t|
